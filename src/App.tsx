19ca5902e8ef639f867b82c859a0aa889a5c330f
pixelg <pixelguerrilla@gmail.com>
2025-03-02 18:44:35 -0600

    Add GroupCard component, dark theme, paths alias, and Storybook
    Introduced a reusable GroupCard component and dark theme support with extended colors and styles. Configured TypeScript paths alias and adjusted Vite to support it. Enhanced development setup with Storybook and updated dependencies for better tooling support.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import {GroupCard} from "@/components/GroupCard.tsx";
 
 const App = () => {
   return (
     <div className='@container bg-light-blue-5 text-dark-gray-20 dark:bg-dark-cyan-3 dark:text-light-gray-2'>
-      <div className='flex justify-center items-center h-screen'>
+      <div className='flex justify-center flex-col items-center h-screen gap-6'>
         <h1 className='text-2xl font-bold inline-block'>Welcome | Home</h1>
+        <GroupCard title={'24-35 Males'} />
       </div>
     </div>
   )
---
+++ src/components/GroupCard.tsx
@@ -0,0 +1,21 @@
+import {
+  Card,
+  CardContent, CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+type CardProps = React.ComponentProps<typeof Card>
+
+export const GroupCard = ({...props}: CardProps) => {
+  return (
+    <Card {...props}>
+      <CardHeader>
+        <CardTitle>{props.title}</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua.</p>
+      </CardContent>
+    </Card>
+  )
+}
